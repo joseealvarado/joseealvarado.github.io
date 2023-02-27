d53839ecd6ac6f27fe0223bf0c114e7659b6e404
Jose Alvarado <124101115+joseealvarado@users.noreply.github.com>
2023-02-27 17:50:27 -0300

    Add files via upload

---
+++ proyecto.js
@@ -0,0 +1,16 @@
+
+function inicio() {
+    location.href = "index.html"
+}
+    function bibliografia() {
+        location.href = "bibliografia.html"
+}
+function acontecimiento() {
+    location.href = "acontecimiento.html"
+}
+function familia() {
+    location.href = "familia.html"
+}
+function proyecto() {
+    location.href = "proyecto.html"
+}
